1f8d9917cd3917a5082f232003abde490a0eb249
math198512 <thoraichi@gmail.com>
2024-02-04 10:41:13 -0800

    test_base_model.py

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+"""
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    def test_save(self):
+        my_model = BaseModel()
+
+        initial_updated_at = my_model.updated_at
+        current_updated_at = my_model.save()
+        self.
