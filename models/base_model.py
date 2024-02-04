555f4ca4e8768f7395ededf2580be953e7019f7b
math198512 <thoraichi@gmail.com>
2024-02-04 08:17:06 -0800

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
 
         self.created_at = datetime.utcnow()
-        self.updated_at = datetime.uctnow()
+        self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """
