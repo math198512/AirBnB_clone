a929a7b082599e64b75f3fc5e03af19da2fc4e32
math198512 <thoraichi@gmail.com>
2024-02-10 09:00:12 -0800

    file_storage.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+This module serializes instances to a JSON file and
+deserializes JSON file to instances
+"""
+
+import json
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        """
+
+        return FileStorage.__objects
+    
+    def new(self, obj):
+        """
+        """
+
+        key = obj.__class__.__name__ + "." + obj.id
+        __class__.__objects[key] = obj
+
+    def save(self):
+        """
+        """
+
+        new_dict = {}
+        for key, value in self.__objects.items():
+            new_dict[key] = value.to_dict()
+
+        with open(FileStorage.__file_path, mode="w", encoding="UTF-8") as wr:
+            json.dump(new_dict, wr)
+
+    def reload(self):
+        """
+        """
+
+        try:
+            with open(FileStorage.__file_path, mode="r",
+                    encoding="UTF-8") as js_file:
+                dict_objs = json.load(js_file)
+                for key, value in dict_objs.items():
+                    class_name, obj_id = key.split('.')
+                    cls = eval(class_name)
+                    inst = cls(**values)
+                    FileStorage.__objects__[key] = inst
+                except Exception:
+                    pass
+
