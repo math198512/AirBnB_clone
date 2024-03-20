d5e656b6b2e48fafe68765088c0351c2f603d2cd
math198512 <thoraichi@gmail.com>
2024-03-20 16:50:19 -0700

    user.py

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ This module defines all attributes/methods for class User
 
 from models.base_model import BaseModel
 
-class User(BaseMedol):
+class User(BaseModel):
     """This class defines a user"""
     email = ''
     password = ''
