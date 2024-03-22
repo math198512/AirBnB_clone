552f5a2afedeb48640b3664d1b831f106c438b27
math198512 <thoraichi@gmail.com>
2024-03-22 14:36:30 -0700

    state.py

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines all attributes/methods for class State
+"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """This class defines a state"""
+    name = ''
