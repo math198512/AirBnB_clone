e9490dd60d9886e9402cf6d934b804fe3e6022aa
math198512 <thoraichi@gmail.com>
2024-03-22 14:45:03 -0700

    amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines all attributes/methods for class Amenity
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """This class defines an amenity"""
+    name = ''
