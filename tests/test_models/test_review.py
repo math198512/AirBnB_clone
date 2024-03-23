658cf23dc6f0fa3b9979830dd557b9aecdf8c5ee
math198512 <thoraichi@gmail.com>
2024-03-23 04:02:00 -0700

    test_review.py

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" """
+from tests.test_models.test_base_model import test_basemodel
+from models.user import User
+from models.review import Review
+
+class test_Review(test_basemodel):
+    """ """
+    
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Review"
+        self.value = Review
