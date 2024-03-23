cc06382c6a9c3a8484e48401daec2aa30d6a1a5a
math198512 <thoraichi@gmail.com>
2024-03-23 03:47:40 -0700

    test_city.py

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" """
+from tests.test_models.test_base_model import test_basemodel
+from models.user import User
+from models.city import City
+
+class test_City(test_basemodel):
+    """ """
+    
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "City"
+        self.value = City
