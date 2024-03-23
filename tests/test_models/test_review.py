43e84c570fa6f8f36c14d742b4d886938bad50c3
math198512 <thoraichi@gmail.com>
2024-03-23 08:12:15 -0700

    test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+from tests.test_models.test_base_model import TestBaseModel
 from models.user import User
 from models.review import Review
 
-class test_Review(test_basemodel):
+class test_Review(TestBaseModel):
     """ """
     
 
