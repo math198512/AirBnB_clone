d787a1d5448ac580713c40101604490cce722711
math198512 <thoraichi@gmail.com>
2024-03-23 03:36:04 -0700

    city.py

--- models/city.py
+++ models/city.py
@@ -6,7 +6,7 @@ This module defines all attributes/methods for class City
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
+class City(BaseModel):
     """This class defines a city"""
     state_id = ''
     name = ''
