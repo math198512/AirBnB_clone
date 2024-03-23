7cf93f0468185b81e729d2dd88e0196418429871
math198512 <thoraichi@gmail.com>
2024-03-23 07:50:42 -0700

    test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+from tests.test_models.test_base_model import TestBaseModel
 from models.user import User
 from models.amenity import Amenity
 
-class test_Amenity(test_basemodel):
+class test_Amenity(TestBaseModel):
     """ """
     
 
@@ -13,3 +13,6 @@ class test_Amenity(test_basemodel):
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
+
+if __name__ == "__main__":
+    unittest.main()
