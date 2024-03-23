b83aa78c706193ffe7de1a13281fb95ce79d021b
math198512 <thoraichi@gmail.com>
2024-03-23 03:32:32 -0700

    place.py

---
+++ models/place.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module defines all attributes/methods for class Place
+"""
+
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """This class defines a place"""
+    city_id = ''
+    user_id = ''
+    name = ''
+    description = ''
+    number_rooms = 0
+    max_guest = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
